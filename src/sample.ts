39f360c894a8cb294dfb0f4acd0dd25846669808
Patrick Lei <patrick.lei.99@gmail.com>
2025-09-22 10:37:45 -0400

    improve seo and accessibility

--- src/sample.ts
+++ src/sample.ts
@@ -1,7 +1,7 @@
 const author = {
     name: `Patrick`,
     nickname: `Notes`,
-    github: `https://www.tiktok.com/@codetonero`,
+    github: `https://www.github.com/plei99`,
     bilibili: `https://space.bilibili.com/3546599313640115`,
     homepage: `https://plei99.github.io/`,
     email: 'mailto:patrick.lei@bc.edu'
